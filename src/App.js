364e1dda6f28f828d5a0af4b3cd31098e017e830
gracekim27 <gracejkim9@gmail.com>
2023-05-05 22:50:09 -0400

    sdfdsf

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Contact from './components/Contact';
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import './index.css';
+
+function Projects() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+    </div>
+  );
+}
+
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <div className='container'>
+      <Router>
+        <header>
+        <Navbar fixed='top' expand='lg' className="navbar-nav">
+          <Nav>
+            <Nav.Link as={Link} to="/">Grace Kim</Nav.Link>
+            <Nav.Link as={Link} to="/projects"> <h4>Projects</h4></Nav.Link>
+            <Nav.Link as={Link} to="/contact"> <h4>Contact</h4></Nav.Link>
+          </Nav>
+        </Navbar>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <img src="../public/meinfrance.png" alt = " "/>
+      <h2>Welcome to my home page</h2>
+      <p>Home page content goes here...</p>
+    </div>
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './App.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
